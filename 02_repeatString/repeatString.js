3a40b8b4163c74d7de6f816eb6871731668c8d0f
nlai <nicholaslhlai@gmail.com>
2023-01-31 23:46:39 -0500

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(userInput, repeatNum) {
 
+    if (repeatNum < 0) {
+        return "ERROR";
+    }
+
+    let res = "";
+    for (let i = 0; i < repeatNum; i++) {
+        res += userInput; 
+    }
+    return res;
 };
 
 // Do not edit below this line
