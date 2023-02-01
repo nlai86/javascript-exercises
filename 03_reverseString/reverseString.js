8c4ca91e17baf74fa2efa1b4a603a38d80713ff4
nlai <nicholaslhlai@gmail.com>
2023-01-31 23:59:24 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(userInput) {
+    let res = [];
+    for (let i = 0; i < userInput.length; i++) {
+        res[i] = userInput.charAt(userInput.length - 1 - i);
+    }
+    res = res.join("");
+    return res;
 };
 
 // Do not edit below this line
